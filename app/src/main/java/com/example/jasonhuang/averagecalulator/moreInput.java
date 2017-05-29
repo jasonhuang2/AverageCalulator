cf12363e34f87b54edf4b4e6462ff4dcb89a631d
jasonhuang2 <jhuang2388@gmail.com>
2017-05-28 22:12:48 -0600

    Test for main pc

---
+++ app/src/main/java/com/example/jasonhuang/averagecalulator/moreInput.java
@@ -0,0 +1,13 @@
+package com.example.jasonhuang.averagecalulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class moreInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_input_activity);
+    }
+}
